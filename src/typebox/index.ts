925297ad8749d247a7174f0ff5ef2ac1ffdf10f2
aberigle <aberigle@uhurus.com>
2025-03-11 12:24:00 +0100

    make types accessible

--- src/typebox/index.ts
+++ src/typebox/index.ts
@@ -1,14 +1,14 @@
-import { type TObject } from "@sinclair/typebox"
+import { TSchema, type TObject } from "@sinclair/typebox"
 import { Model } from "./model"
 
-export function fromTypebox(
+export function fromTypebox<T extends TSchema>(
   db: any,
-  object: TObject,
+  object: T,
   name: string | undefined = object.$id,
 ) {
   if (!name) throw new Error("Missing $id or name")
 
-  return new Model(
+  return new Model<T>(
     db,
     name,
     object
